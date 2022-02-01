e1b2fedccbdaaeb64c7dca47c124489f0e9b6f25
JenniferDominique <j.dominiqueds@gmail.com>
2022-01-31 22:07:00 -0300

    Vendo a palavra sendo formada a cada letra acertada

--- Forca/forca.c
+++ Forca/forca.c
@@ -7,21 +7,44 @@ void main(){
     sprintf(palavraSecreta, "MELANCIA"); //atribuindo a string ao array
 
     int acertou = 0;
-    int enforcou = 1;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
 
     do{
-        char chute;
-        printf("Chute uma letra: ");
-        scanf("%c", &chute);
-
-        // Varrendo a string para ver se tem a letra do chute
         int tamanhoPalavra = strlen(palavraSecreta); // strlen - utiliza #include <string.h>
-        
+
+        // Formando a palavra na tela
         for(int i = 0; i < tamanhoPalavra; i++){
-            if(chute == palavraSecreta[i]){
-                printf("A posicao %d tem essa letra!\n", i);
+
+            int achou = 0;
+
+            // Varrendo o array com todas as letras chutadas
+            for(int j = 0; j < tentativas; j++){
+                if(chutes[j] == palavraSecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if(achou){
+                printf("%c ", palavraSecreta[i]);
+            } else{
+                printf("_ ");
             }
         }
+
+        printf("\n\n");
+        
+        char chute;
+        printf("Chute uma letra: ");
+        scanf(" %c", &chute); 
+        //Dica: coloca um espaco antes do %c para ignorar o caractere de enter
+
+        // Guardando as letras ja chutadas
+        chutes[tentativas] = chute;
+        tentativas++;
 
     } while(!acertou && !enforcou);
 }
