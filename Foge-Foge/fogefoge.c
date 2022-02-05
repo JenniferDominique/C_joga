00920bded9ad601e3d524f4ec364c7c16935d55c
JenniferDominique <j.dominiqueds@gmail.com>
2022-02-05 11:44:49 -0300

    Declarando quantidade de linhas e colunas do mapa

--- Foge-Foge/fogefoge.c
+++ Foge-Foge/fogefoge.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
-    char mapa[5][10+1]; // Matriz = linha x coluna
-    // +1 por causa do \0 do final da string
+//VARIAVEIS GLOBAIS
+char mapa[5][10+1]; // Matriz = linha x coluna -> +1 por causa do \0 do final da string
+int linhas;
+int colunas;
 
+void main(){
+    
     // Abrindo o arquivo de mapa
     FILE* f;
     f = fopen("mapa.txt", "r");
-
     // Se der erro no acesso ao arquivo
     if(f == 0){
         printf("Erro na leitura do mapa!\n");
         exit(1); // Fecha o programa
     }
+
+    fscanf(f, "%d %d", &linhas, &colunas);
+    printf("Linhas: %d, Colunas: %d\n", linhas, colunas);
 
     for(int i = 0; i < 5; i++){
         fscanf(f, "%s", mapa[i]); // Pegando por linha
