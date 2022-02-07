eaff46d92de5c29873853183dc93984cd565cf7c
JenniferDominique <j.dominiqueds@gmail.com>
2022-02-07 20:34:04 -0300

    Fibonacci recursivo

---
+++ FibonacciRecursivo.c
@@ -0,0 +1,9 @@
+// So para anotar de como seria a
+//funcao de Fibonacci recursiva
+
+int fibonacci(int num){
+    if(num == 0) return 0;
+    if(num == 1) return 1;
+
+    return fibonacci(num-1) + fibonacci(num-2);
+}
